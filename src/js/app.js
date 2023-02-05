7d51801f089730293c1455779b65de1467b97055
VangelKachulev <vangel.kachulev@abv.bg>
2023-02-05 13:52:46 +0200

    even numbers list

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,18 @@
 import "../scss/app.scss";
-const R = require("ramda");
-window.addEventListener("DOMContentLoaded", () => {
-  // This block will be executed once the page is loaded and ready
 
-  const ul = document.querySelector("ul");
-  const numbersArray = [1, 3, 4, 5, 6, 7];
-  const isEven = (n) => n % 2 === 0;
-  const filteredArray = R.filter(isEven, numbersArray);
+window.addEventListener("DOMContentLoaded", () => {
+    // This block will be executed once the page is loaded and ready
+
+    const ul = document.querySelector("ul");
+    const numbersArray = [1, 3, 4, 5, 6, 7];
+
+    const filteredArray = numbersArray.filter(n => {
+        return n % 2 == 0
+    });
+    filteredArray.forEach(n => {
+        let liElement = document.createElement('li');
+        liElement.textContent = n;
+        ul.appendChild(liElement);
+    })
+    console.log(filteredArray);
 });
